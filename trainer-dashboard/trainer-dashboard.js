9a95e54453359148331ac848878d960d23420e30
kent-paglomotan <kent.paglomotan@gmail.com>
2021-07-30 14:35:31 -0500

    Added Tab Functionality

--- trainer-dashboard/trainer-dashboard.js
+++ trainer-dashboard/trainer-dashboard.js
@@ -1,1 +1,20 @@
 // This is where the javascript would go
+// Select the elements that contains the tab
+const tabs = document.querySelectorAll('[data-tab-target]');
+const tabContent = document.querySelectorAll('[data-tab-content]');
+// Loops through the elements every time it's click, show the content
+tabs.forEach(tab => {
+    tab.addEventListener('click', () =>{
+        // Get the tab element dashboard, statistics, settings
+        const target = document.querySelector(tab.dataset.tabTarget);
+        // Remove active class
+        tabContent.forEach(tabContent => {
+            tabContent.classList.remove('active')
+        });
+        tabs.forEach(tab => {
+            tab.classList.remove('active')
+        });
+        tab.classList.add("active");
+        target.classList.add("active");
+    });
+});
