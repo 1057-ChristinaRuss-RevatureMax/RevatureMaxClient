9845152149831616932f033c3346184537b10bae
mannyhi <mannyhinn@gmail.com>
2021-08-06 14:04:51 -0400

    loading circle

---
+++ loadingcircle/loadingcircle.js
@@ -0,0 +1,10 @@
+var bar = new ProgressBar.Circle(container, {
+  strokeWidth: 6,
+  easing: 'easeInOut',
+  duration: 10000,  // 10 seconds
+  color: '#F26925',
+  trailColor: '#B9B9BA',
+  trailWidth: 1,
+  svgStyle: null
+});
+bar.animate(1.0);
